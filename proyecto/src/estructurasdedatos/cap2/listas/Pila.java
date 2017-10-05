4fc54922ce98d706ba1bfc379616a2f370abdf8a
Manuel Prada <manuel-.prada@utp.edu.co>
2017-10-05 16:20:22 -0500

    de nuevo

--- proyecto/src/estructurasdedatos/cap2/listas/Pila.java
+++ proyecto/src/estructurasdedatos/cap2/listas/Pila.java
@@ -4,11 +4,79 @@
  * and open the template in the editor.
  */
 package estructurasdedatos.cap2.listas;
-
+import estructurasdedatos.utiles.nodos.NodoSimple;
 /**
  *
- * @author tusk
+ * @author Manuel Prada
  */
 public class Pila {
     
+private NodoSimple dato;
+private NodoSimple tope;
+    
+    
+    
+    public NodoSimple getDato() {
+        return dato;
+    }
+
+
+    public void setDato(NodoSimple dato) {
+        this.dato = dato;
+    }
+
+    
+    public Pila(){
+        dato = null;
+       
+    }
+
+    public boolean vacia(){
+        return (tope == null);
+    }
+    
+    
+    
+       public void apilar(String nombre, String fecha, String responsable){
+        NodoSimple nuevo =new NodoSimple();
+       
+        if(vacia()){
+            tope=nuevo;
+        }else{
+        nuevo.setSiguiente(tope);
+        tope = nuevo;
+
+        }
+        
+    }
+       
+       public void desapilar() {
+       
+        if (vacia()) {
+          
+            System.out.println("No existen elementos en la pila");
+        } else {
+           
+           
+            tope = tope.getSiguiente();
+
+        }
+    }
+   
+       
+        public void listar() {
+       
+        if (!(tope == null)) {
+           
+            NodoSimple aux = tope;
+            
+            System.out.println("LA PILA SE CONFORMA POR");
+            while (aux != null) {
+
+
+                aux = aux.getSiguiente();
+            }
+        }
+
+    }
 }
