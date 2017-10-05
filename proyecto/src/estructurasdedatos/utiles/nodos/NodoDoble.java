db9c10e83b378ed3cd8cb8e607fcaa8d53351dab
Manuel Prada <manuel-.prada@utp.edu.co>
2017-10-05 15:59:30 -0500

    de nuevo

--- proyecto/src/estructurasdedatos/utiles/nodos/NodoDoble.java
+++ proyecto/src/estructurasdedatos/utiles/nodos/NodoDoble.java
@@ -7,8 +7,44 @@ package estructurasdedatos.utiles.nodos;
 
 /**
  *
- * @author tusk
+ * @author Manuel Prada
  */
 public class NodoDoble {
     
+
+    private int dato;
+    private NodoDoble anterior;
+    private NodoDoble siguiente;
+
+ 
+    public int getDato() {
+        return dato;
+    }
+   
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+  
+    public NodoDoble getAnterior() {
+        return anterior;
+    }
+   
+    public void setAnterior(NodoDoble anterior) {
+        this.anterior = anterior;
+    }
+ 
+    public NodoDoble getSiguiente() {
+        return siguiente;
+    }
+  
+    public void setSiguiente(NodoDoble siguiente) {
+        this.siguiente = siguiente;
+    }
+    public void crearnodoListadoble (){
+        
+        this.siguiente=null;
+        this.anterior=null;
+        this.dato=0;
+        
+    } 
 }
