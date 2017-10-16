1322a5dbea324a374b0937b6aa69dadae7917585
Manuel Prada <manuel-.prada@utp.edu.co>
2017-10-16 17:05:28 -0500

    terminadno trabajo de entrega

--- proyecto/src/estructurasdedatos/utiles/nodos/NodoMapa.java
+++ proyecto/src/estructurasdedatos/utiles/nodos/NodoMapa.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package estructurasdedatos.cap2.listas;
+package estructurasdedatos.utiles.nodos;
 
 /**
  *
- * @author ManuelPrada
+ * @author manuel
  */
-public class MatrizDispersa {
+public class NodoMapa {
     
 }
