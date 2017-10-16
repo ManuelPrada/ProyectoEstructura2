1c157d59c142e39b3d9ec0f7ef6bbe59bd33ae83
Manuel Prada <manuel-.prada@utp.edu.co>
2017-10-16 17:09:37 -0500

    terminando el trabajo de entrega

--- proyecto/src/estructurasdedatos/cap3/mapas/Mapa.java
+++ proyecto/src/estructurasdedatos/cap3/mapas/Mapa.java
@@ -9,6 +9,19 @@ package estructurasdedatos.cap3.mapas;
  *
  * @author tusk
  */
-public class Mapa {
+public class Mapa <CV,VL> {
+    
+   private CV clave; 
+   private VL valor;
+   private Mapa <CV,VL> next;
+   
+   public Mapa (CV clave,VL valor) {
+       this.clave = clave;
+       this.valor = valor;
+   }
+   
+   public CV getClave (){
+       return clave;
+   }
     
 }
