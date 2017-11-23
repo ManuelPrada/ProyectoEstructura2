24a00473ab73a050ced9e1fdd3dbf48d7805dce4
Manuel Prada <manuel-.prada@utp.edu.co>
2017-11-23 12:31:25 -0500

    profe en o quededamos
    habia hbalado con usted y me dijo que subiera lo que tenia  ya que en eso habiamos quedado  ya que no podia terminarlo por que e habia pasado el tiempo de entrega

--- proyecto/src/estructurasdedatos/cap4/arboles/ArbolAVL.java
+++ proyecto/src/estructurasdedatos/cap4/arboles/ArbolAVL.java
@@ -4,11 +4,63 @@
  * and open the template in the editor.
  */
 package estructurasdedatos.cap4.arboles;
-
+import estructurasdedatos.utiles.nodos.NodoArbolAVL;
 /**
  *
  * @author tusk
  */
 public class ArbolAVL {
-    
+      
+    public NodoArbolAVL raiz;
+ 
+     
+    public ArbolAVL( int valor ) {
+        this.raiz = new NodoArbolAVL (  );
+    }
+ 
+    public ArbolAVL( NodoArbolAVL raiz ) {
+        this.raiz = raiz;
+    }
+ 
+ 
+    public NodoArbolAVL getRaiz() {
+        return raiz;
+    }
+ 
+    public void setRaiz(NodoArbolAVL raiz) {
+        this.raiz = raiz;
+    }
+             
+  
+    private void Insertar(NodoArbolAVL nodo, NodoArbolAVL raiz ) {
+        
+        if ( raiz == null ) {
+            
+            this.setRaiz(nodo);
+        }
+        else {           
+            if ( nodo.getValor() <= raiz.getValor() ) {              
+                if (raiz.getHijoDerecho() == null) {
+                    raiz.setHijoIzquierdo(nodo);
+                }
+                else {
+                    Insertar( nodo , raiz.getHijoIzquierdo() );
+                }
+            }
+            else {              
+                if (raiz.getHijoDerecho() == null) {
+                    raiz.setHijoDerecho(nodo);
+                }
+                else {
+                    Insertar( nodo, raiz.getHijoDerecho());
+                }
+            }
+        }
+        
+                 raiz.calcularaltura(raiz,0);
+                 
+                 
+        
+                  
+}
 }
--- proyecto/src/estructurasdedatos/utiles/nodos/NodoArbolAVL.java
+++ proyecto/src/estructurasdedatos/utiles/nodos/NodoArbolAVL.java
@@ -10,5 +10,109 @@ package estructurasdedatos.utiles.nodos;
  * @author tusk
  */
 public class NodoArbolAVL {
-    
+
+    private int valor;
+    private NodoArbolAVL dato;
+    private NodoArbolAVL hijoIzquierdo;
+    private NodoArbolAVL hijoDerecho;
+    private NodoArbolAVL factorequilibrio;
+    private int alturaArbol = 1;
+
+    /**
+     * @return the valor
+     */
+    public int getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the dato
+     */
+    public NodoArbolAVL getDato() {
+        return dato;
+    }
+
+    /**
+     * @param dato the dato to set
+     */
+    public void setDato(NodoArbolAVL dato) {
+        this.dato = dato;
+    }
+
+    /**
+     * @return the hijoIzquierdo
+     */
+    public NodoArbolAVL getHijoIzquierdo() {
+        return hijoIzquierdo;
+    }
+
+    /**
+     * @param hijoIzquierdo the hijoIzquierdo to set
+     */
+    public void setHijoIzquierdo(NodoArbolAVL hijoIzquierdo) {
+        this.hijoIzquierdo = hijoIzquierdo;
+    }
+
+    /**
+     * @return the hijoDerecho
+     */
+    public NodoArbolAVL getHijoDerecho() {
+        return hijoDerecho;
+    }
+
+    /**
+     * @param hijoDerecho the hijoDerecho to set
+     */
+    public void setHijoDerecho(NodoArbolAVL hijoDerecho) {
+        this.hijoDerecho = hijoDerecho;
+    }
+
+    /**
+     * @return the factorequilibrio
+     */
+    public NodoArbolAVL getFactorequilibrio() {
+        return factorequilibrio;
+    }
+
+    /**
+     * @param factorequilibrio the factorequilibrio to set
+     */
+    public void setFactorequilibrio(NodoArbolAVL factorequilibrio) {
+        this.factorequilibrio = factorequilibrio;
+    }
+
+    /**
+     * @return the alturaArbol
+     */
+    public int getAlturaArbol() {
+        return alturaArbol;
+    }
+
+    /**
+     * @param alturaArbol the alturaArbol to set
+     */
+    public void setAlturaArbol(int alturaArbol) {
+        this.alturaArbol = alturaArbol;
+    }
+
+    public void calcularaltura(NodoArbolAVL raiz, int contador) {
+
+        if (raiz != null) {
+            calcularaltura(raiz.hijoIzquierdo, contador + 1);
+
+            calcularaltura(raiz.hijoDerecho, contador + 1);
+
+            //raiz.alturaArbol= Math.max(, alturaderecha);//
+            System.out.println(contador);
+        }
+
+    }
+
 }
